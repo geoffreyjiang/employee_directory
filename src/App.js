605efe0c94e6e807fede7560a4bc72d848d36289
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-03-03 11:20:23 -0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Card from "./Components/Card/Card";
 import User from "./Components/User/User";
+import Nav from "./Components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       {/* <Card /> */}
       <User />
     </div>
